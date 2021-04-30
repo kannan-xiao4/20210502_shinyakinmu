88cdc9196824ebe6c53109c4bd1200743a3ecd64
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-04-30 17:11:48 +0900

    update

--- Assets/Scripts/BreadFactory.cs
+++ Assets/Scripts/BreadFactory.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BreadFactory : MonoBehaviour
 {
-    [SerializeField] private Transform parent;
+    [SerializeField] private Transform laneParent;
+    [SerializeField] private List<GameObject> labelSource;
     [SerializeField] private List<GameObject> breadSource;
-
+    [SerializeField] private List<GameObject> labeledBreadSource;
+    
     private void Start()
     {
     }
