d5487cebf5543d9fc215ccb4a1bc2aa868e06443
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-05-02 19:16:38 +0900

    alpha

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,5 +1,7 @@
 using System;
 using UniRx;
+using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Manager
 {
@@ -11,7 +13,7 @@ namespace Manager
         {
             _soundManager = soundManager;
         }
-        
+
         private ReactiveProperty<int> creamBreadScore = new ReactiveProperty<int>(0);
         private ReactiveProperty<int> redBeansBreadScore = new ReactiveProperty<int>(0);
         private ReactiveProperty<int> scrapScore = new ReactiveProperty<int>(0);
@@ -24,6 +26,31 @@ namespace Manager
         public IObservable<int> ScrapScore => scrapScore;
 
         public IObservable<int> FailedScore => failedScore;
+
+
+        private int wageScore = 0;
+        
+        public IObservable<int> WageScore
+        {
+            get
+            {
+                return creamBreadScore
+                    .CombineLatest(redBeansBreadScore, (cream, redbeans) => cream + redbeans)
+                    .CombineLatest(FailedScore, (bread, failed) =>
+                    {
+                        wageScore = bread * 10;
+                        wageScore -= failed * 10;
+
+                        var hundredBonusCount = bread / 100;
+                        var tenBonusCount = bread / 10 - hundredBonusCount;
+
+                        wageScore += hundredBonusCount * 1000;
+                        wageScore += tenBonusCount * 100;
+
+                        return wageScore;
+                    });
+            }
+        }
 
         public void AddCreamBreadScore()
         {
@@ -47,17 +74,35 @@ namespace Manager
         }
 
         private Subject<Unit> gameStart = new Subject<Unit>();
+        private Subject<Unit> gameEnd = new Subject<Unit>();
 
         public IObservable<Unit> GameStart => gameStart;
+        public IObservable<Unit> GameEnd => gameEnd;
 
-        public void Start()
+        public void StartGame()
         {
             gameStart.OnNext(Unit.Default);
         }
-        
-        public void Exit()
+
+        public void EndGame()
         {
             _soundManager.PlaySE(SE.Exit);
+            gameEnd.OnNext(Unit.Default);
+        }
+
+        public void ResetGame()
+        {
+            creamBreadScore.Value = 0;
+            redBeansBreadScore.Value = 0;
+            scrapScore.Value = 0;
+            failedScore.Value = 0;
+            wageScore = 0;
+        }
+
+        public void Report()
+        {
+            var tweet = $"夜勤で{wageScore:#,0}円稼ぎました。 #深夜勤務 https://unityroom.com/games/shinyakinmu2";
+            Application.OpenURL ("https://twitter.com/intent/tweet?text=" + UnityWebRequest.EscapeURL(tweet));
         }
     }
 }
--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -9,12 +9,30 @@ namespace Manager
 {
     public class UIManager : MonoBehaviour
     {
+        [SerializeField] private GameObject title;
+        [SerializeField] private GameObject inGame;
+        [SerializeField] private GameObject result;
+
+        // title object
+        [SerializeField] private Button titleButton;
+
+        // inGame object
         [SerializeField] private Text creamBreadScore;
         [SerializeField] private Text redBeansBreadScore;
         [SerializeField] private Text scrapScore;
         [SerializeField] private Text failedScore;
         [SerializeField] private Button exitButton;
-        [SerializeField] private Animator animator;
+        [SerializeField] private Animator startAnimator;
+
+        // result object
+        [SerializeField] private Text creamBreadResultScore;
+        [SerializeField] private Text redBeansBreadResultScore;
+        [SerializeField] private Text scrapResultScore;
+        [SerializeField] private Text failedResultScore;
+        [SerializeField] private Text wageResultScore;
+        [SerializeField] private Button reportButton;
+        [SerializeField] private Button returnTitleButton;
+
 
         private GameManager _manager;
         private SoundManager _soundManager;
@@ -28,35 +46,59 @@ namespace Manager
 
         private void Start()
         {
+            titleButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    startAnimator.gameObject.SetActive(true);
+                    startAnimator.Play("StartAnimation");
+                    title.SetActive(false);
+                }).AddTo(this);
+
+
+            InitInGame();
+            InitResult();
+        }
+
+        private void InitInGame()
+        {
             _manager.CreamBreadScore.SubscribeToText(creamBreadScore).AddTo(this);
             _manager.RedBeansBreadScore.SubscribeToText(redBeansBreadScore).AddTo(this);
             _manager.ScrapScore.SubscribeToText(scrapScore).AddTo(this);
             _manager.FailedScore.SubscribeToText(failedScore).AddTo(this);
-            exitButton.OnClickAsObservable().Subscribe(_ => _manager.Exit()).AddTo(this);
-
-            this.UpdateAsObservable()
-                .Where(_ => Input.anyKeyDown)
-                .Take(1)
-                .Subscribe(_ => animator.Play("StartAnimation"))
-                .AddTo(this);
-
-            if (!animator.gameObject.activeInHierarchy)
+            exitButton.OnClickAsObservable().Subscribe(_ =>
             {
-                _manager.Start();
-                return;
-            }
-            
-            var stateMachineTrigger = animator.GetBehaviour<ObservableStateMachineTrigger>();
+                _manager.EndGame();
+                result.SetActive(true);
+            }).AddTo(this);
+
+            var stateMachineTrigger = startAnimator.GetBehaviour<ObservableStateMachineTrigger>();
+
             stateMachineTrigger.OnStateEnterAsObservable()
                 .Subscribe(_ => _soundManager.PlayBGM(BGM.Start))
                 .AddTo(this);
             stateMachineTrigger.OnStateExitAsObservable()
                 .Subscribe(_ =>
                 {
-                    _manager.Start();
+                    _manager.StartGame();
                     _soundManager.PlayBGM(BGM.Others);
-                    animator.gameObject.SetActive(false);
+                    startAnimator.gameObject.SetActive(false);
                 }).AddTo(this);
+        }
+
+        private void InitResult()
+        {
+            _manager.CreamBreadScore.SubscribeToText(creamBreadResultScore).AddTo(this);
+            _manager.RedBeansBreadScore.SubscribeToText(redBeansBreadResultScore).AddTo(this);
+            _manager.ScrapScore.SubscribeToText(scrapResultScore).AddTo(this);
+            _manager.FailedScore.SubscribeToText(failedResultScore).AddTo(this);
+            _manager.WageScore.Select(wage => $"{wage}円").SubscribeToText(wageResultScore).AddTo(this);
+            reportButton.OnClickAsObservable().Subscribe(_ => _manager.Report()).AddTo(this);
+            returnTitleButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                _manager.ResetGame();
+                title.SetActive(true);
+                result.SetActive(false);
+            }).AddTo(this);
         }
     }
 }
